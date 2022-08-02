b36289926ef31a5c2ca980d84528e51efbfaab9f
Weiju Wang <weijuwang@aol.com>
2022-08-02 14:56:16 -0700

    Fixed formatting to be more consistent

--- webserver.c
+++ webserver.c
@@ -18,55 +18,89 @@ struct monRequest
     char body[BUFFER]; // unbounded cuz who cares
 };
 
-struct webHeader{
+struct webHeader
+{
     char key[BUFFER];
     char value[BUFFER];
 };
 
 struct webRequest
 {
-    char uri[BUFFER]; char method[BUFFER]; char version[BUFFER]; char *body;
+    char uri[BUFFER];
+    char method[BUFFER];
+    char version[BUFFER];
+    char *body;
     struct webHeader headers[20];//fix needed
 };
+
+const char *newline = "\r\n";
+const char *twoNewlines = "\r\n\r\n";
 
 //functions to send text and file
 //gotta figure out how to receive file, as well as multipart
 
-int sendHeader(char* key, char* value, int new_fd){
+int sendHeader(char *key, char *value, int new_fd)
+{
     char resp[BUFFER] = "HTTP/1.0 200 OK\r\n"
                   "Server: webserver-c\r\n";
-    if(send(new_fd, resp, strlen(resp), 0)== -1){perror("header sending");return 1;}
+
+    if(send(new_fd, resp, strlen(resp), 0)== -1)
+    {
+        perror("header sending");
+        return 1;
+    }
+
     send(new_fd, key, strlen(key), 0);
     send(new_fd, ":", strlen(":"), 0);
-    send(new_fd, "\r\n\r\n", strlen("\r\n\r\n"), 0);
+    send(new_fd, twoNewLines, strlen(twoNewLines), 0);
     return 0;
 }
 
-int sendText(char *textToSend, int new_fd){
+int sendText(char *textToSend, int new_fd)
+{
     sendHeader("Content-Type", " text/plain", new_fd);
     int bytes_sent = send(new_fd, textToSend, strlen(textToSend), 0);
-    if(bytes_sent == -1){
+
+    if(bytes_sent == -1)
+    {
         perror("error sending text");
         return 1;
     }
-    send(new_fd, "\r\n", strlen("\r\n"), 0);
+
+    send(new_fd, newline, strlen(newline), 0);
     return 0;
 }
 
-int sendFileWeb(char *filename, char *contentType, int new_fd){
+int sendFileWeb(char *filename, char *contentType, int new_fd)
+{
     FILE *fp;
     fp = fopen(filename, "r");
-    if(fseek(fp, 0L, SEEK_END) != 0){perror("seeking");return 1;};
+
+    if(fseek(fp, 0L, SEEK_END) != 0){
+        perror("seeking");
+        return 1;
+    }
+
     int sz = ftell(fp);
     char ch[sz];
     rewind(fp);
-    if(fread(ch, 1,sz,  (FILE *)fp) < 0){perror("reading");return 1;}
-    if(sendHeader("Content-Type", contentType, new_fd) != 0){return 1;}
-    if(send(new_fd, ch, sz, 0)==-1){perror("sending file"); return 1;}
+
+    if(fread(ch, 1,sz,  (FILE *)fp) < 0){
+        perror("reading");
+        return 1;
+    }
+
+    if(sendHeader("Content-Type", contentType, new_fd) != 0)
+        return 1;
+
+    if(send(new_fd, ch, sz, 0) == -1)
+    {
+        perror("sending file");
+        return 1;
+    }
+
     return 0;
 }
-
-
 
 //add other stuff
 
@@ -101,30 +135,31 @@ struct webRequest parseWebRequest(char *total){
     sscanf(total, "%s %s %s", returned.method, returned.uri, returned.version);
 
     //get everything after and incuding carriage return
-    returned.body = strstr(total, "\r\n\r\n");
+    returned.body = strstr(total, twoNewLines);
     //remove carriage return
-    total += (strlen(returned.method) + strlen(returned.uri)+ strlen(returned.version) + 4);
+    total += (strlen(returned.method) + strlen(returned.uri) + strlen(returned.version) + 4);
     total[strlen(total) - strlen(returned.body)] = '\0';
     returned.body += 4;
 
     struct webHeader headers[20];
-    char* context = NULL;
+    char *context = NULL;
     char *token;
     int lastentry = 0;
-    token = strtok_r(total, "\r\n", &context);
+
+    token = strtok_r(total, newline, &context);
     while (token != NULL)
     {
         kVParse(token, headers[lastentry].key, headers[lastentry].value, 1);
-        token = strtok_r(NULL, "\r\n", &context);
+        token = strtok_r(NULL, newline, &context);
 
         lastentry++;
 
     }
-    memcpy(returned.headers, headers, sizeof(headers));
+
+    memcpy(returned.headers, headers, sizeof headers);
 
     return returned;
 }
-
 
 // two mon
 int monRead(struct monRequest reqsArr[20])
@@ -132,7 +167,7 @@ int monRead(struct monRequest reqsArr[20])
     // i think i need to use pointers for this and monappend
     FILE *fp;
     char ch[BUFFER];
-    char* context = NULL;
+    char *context = NULL;
     char *token;
     // allocate memory
     fp = fopen("requests.mon", "r");
@@ -206,34 +241,24 @@ int main()
     hints.ai_flags = AI_PASSIVE;
 
     if ((status = getaddrinfo(NULL, "4546", &hints, &servinfo) != 0))
-    {
         perror(gai_strerror(status));
-    }
 
     if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1)
-    {
         perror("problem creating socket");
-    }
 
     // sorry joe, adress isnt already in use
     int yes = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
-    {
         perror("error fixing other error");
-    }
 
     if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) != 0)
-    {
         perror("Problem binding socket");
-    }
 
     // servinfo is not needed anymore
     freeaddrinfo(servinfo);
 
     if (listen(sockfd, BACKLOG) != 0)
-    {
         perror("Problem listening");
-    }
 
     while (1)
     {
@@ -249,10 +274,9 @@ int main()
         FILE *fp = fopen("circles.jpeg", "wb");
         fwrite(req.body, recieved, 1, fp);
         fclose(fp);
+
         if (strcmp(uri, "/") == 0)
-        {
             sendText("Hello, my fellow internet users!", new_fd);
-        }
         else if (strcmp(uri, "/vader") == 0)
         {
             char *msgd = "James earl Jones sent for me";
@@ -260,15 +284,9 @@ int main()
             bytes_sent = send(new_fd, msgd, lenm, 0);
         }
         else if (strcmp(req.uri, "/http") == 0)
-        {
             sendFileWeb("Dance_dance_dance_by_chic_edited_version_US_single_Atlantic.png", " image/jpeg", new_fd);
-        }
-
         else
-        {
-
             bytes_sent = send(new_fd, "404", strlen("404"), 0);
-        }
 
         close(new_fd);
     }
@@ -276,5 +294,4 @@ int main()
     // of the above shoudl be error checked, returns 0/1 if no problem
 
     return 0;
-
 }
